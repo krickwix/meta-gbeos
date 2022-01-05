72e11fa83fd22e7bae009a8b8f005986e85d2652
Fabien Andrieux <fandrieu@cisco.com>
2022-01-05 16:29:00 +0100

    CI: Add jenkins pipelines for images

---
+++ build-jetson-nano-devkit.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent { label 'yocto'}
+    stages {
+        stage('scm') {
+            steps {
+                cleanWs()
+                git branch: 'develop', url: 'https://github.com/krickwix/meta-gbeos.git'
+                withEnv(['all_proxy=http://proxy.esl.cisco.com:80','GIT_TRACE_PACKET=true']) {
+                    sh("all_proxy=http://proxy.esl.cisco.com:80 git submodule update --init --recursive --jobs 32")
+                }
+            }
+        }
+        stage("build") {
+            steps {
+                sh("cd tegra-distro && . setup-env --machine jetson-nano-devkit-emmc --distro tegrademo && bitbake demo-image-full")
+            }
+        }
+        stage("artefacts") {
+            steps {
+                archiveArtifacts artifacts: 'tegra-distro/build/tmp/deploy/images/**/*.tegraflash.tar.gz',
+                   allowEmptyArchive: true,
+                   fingerprint: true,
+                   onlyIfSuccessful: true
+                minio bucket: 'gbear-yocto-images', 
+                   credentialsId: 'minio_gbear-user',
+                   targetFolder: 'jenkins-build/',
+                   host: 'http://10.60.16.240:9199', 
+                   includes: 'tegra-distro/build/tmp/deploy/images/**/*.tegraflash.tar.gz'
+            }
+        }
+    }
+    post {
+        // Clean after build
+        always {
+            cleanWs(cleanWhenNotBuilt: false,
+                    deleteDirs: true,
+                    disableDeferredWipeout: true,
+                    notFailBuild: true,
+                    patterns: [[pattern: '.gitignore', type: 'INCLUDE'],
+                               [pattern: '.propsfile', type: 'EXCLUDE']])
+        }
+    }
+}
---
+++ build-rpi4.groovy
@@ -0,0 +1,54 @@
+pipeline {
+    agent { label 'yocto'}
+    stages {
+        stage('scm') {
+            steps {
+                cleanWs()
+                git branch: 'develop', url: 'https://github.com/krickwix/meta-gbeos.git'
+                withEnv(['LANG=C','all_proxy=http://proxy.esl.cisco.com:80','GIT_TRACE_PACKET=true']) {
+                    sh("all_proxy=http://proxy.esl.cisco.com:80 git submodule update --init --jobs 16")
+                }
+            }
+        }
+        stage("build") {
+            steps {
+                withEnv(['LANG=C']) {
+                    sh("cd rpi-distro && . setupenv && bitbake gbeos-dev")
+                }
+            }
+        }
+        stage("artefacts") {
+            steps {
+                archiveArtifacts artifacts: 'rpi-distro/build/tmp/deploy/images/**/*.wic.bz2',
+                   allowEmptyArchive: true,
+                   fingerprint: true,
+                   onlyIfSuccessful: true
+                archiveArtifacts artifacts: 'rpi-distro/build/tmp/deploy/images/**/*.bmap',
+                   allowEmptyArchive: true,
+                   fingerprint: true,
+                   onlyIfSuccessful: true
+                minio bucket: 'gbear-yocto-images', 
+                   credentialsId: 'minio_gbear-user',
+                   targetFolder: 'jenkins-build/',
+                   host: 'http://10.60.16.240:9199', 
+                   includes: 'rpi-distro/build/tmp/deploy/images/**/*.bmap'
+                minio bucket: 'gbear-yocto-images', 
+                   credentialsId: 'minio_gbear-user',
+                   targetFolder: 'jenkins-build/',
+                   host: 'http://10.60.16.240:9199', 
+                   includes: 'rpi-distro/build/tmp/deploy/images/**/*.wic.bz2'
+            }
+        }
+    }
+    post {
+        // Clean after build
+        always {
+            cleanWs(cleanWhenNotBuilt: false,
+                    deleteDirs: true,
+                    disableDeferredWipeout: true,
+                    notFailBuild: true,
+                    patterns: [[pattern: '.gitignore', type: 'INCLUDE'],
+                               [pattern: '.propsfile', type: 'EXCLUDE']])
+        }
+    }
+}
